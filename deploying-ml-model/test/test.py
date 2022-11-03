1393dd2717a4b28b8f6ed721a0d5f5fb4a020805
Joe-at-LA <36517206+Joe-at-LA@users.noreply.github.com>
2022-11-03 09:17:31 -0400

    Update test.py

--- deploying-ml-model/test/test.py
+++ deploying-ml-model/test/test.py
@@ -1,5 +1,5 @@
 import requests
 
-resp = requests.post("https://getprediction-tqc5taiqdq-lm.a.run.app", files={'file': open('eight.png', 'rb')})
+resp = requests.post("[CLOUD_RUN_ENDPOINT]", files={'file': open('eight.png', 'rb')})
 
 print(resp.json())
